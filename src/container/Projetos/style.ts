5e0d7922b155deb0edd8d3463ca1f7f0f97e20c0
lucas <lucasroque545@gmail.com>
2024-09-19 15:20:04 -0300

    Project designer complet 1.0

--- src/container/Projetos/style.ts
+++ src/container/Projetos/style.ts
@@ -3,7 +3,7 @@ import {  styled } from "styled-components";
 export const Container = styled.section`
 @media screen and (min-width: 1025px) {
   width:100%;
-    max-width:1346px;
+    max-width:1211px;
     margin:0 auto;
     .ProjectList{
         align-items:center;
